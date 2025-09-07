9fe360e5ef0a66ed35f2744af13f591349949f29
Khoi <kn24524@gmail.com>
2025-09-07 15:34:13 +0700

    feat: add custom API error handling utility

---
+++ be/src/utils/ApiError.ts
@@ -0,0 +1,16 @@
+import { AppError } from '../types';
+
+class ApiError extends Error implements AppError {
+  public readonly statusCode: number;
+  public readonly isOperational: boolean;
+
+  constructor(message: string, statusCode: number = 500, isOperational: boolean = true) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export default ApiError;
